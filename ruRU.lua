3863f9b1fc9fdbf6d22797eb5f831d8f1645a669
Hubbotu <34053496+Hubbotu@users.noreply.github.com>
2020-07-09 14:31:30 +0300

    Add files via upload

---
+++ ruRU.lua
@@ -0,0 +1,26 @@
+local ADDON, Package = ...
+
+Package.ruRU = {
+  change_shaman_class_color = "Заклинания шамана отображены синим цветом",
+  use_arrow_keys_in_chat = "Используйте стрелки влево-вправо для перемещения курсора в чате",
+  increase_chat_history = "Увеличьте историю чата до 4096 строк",
+  unclamp_chat_frame = "Разрешить окну чата переместить край экрана",
+  use_sticky_raid_warning_in_chat = "Сохранить \"/rw\" состояние во время чата",
+  use_class_color_in_chat = "Используйте цвет класса в чате",
+  use_max_zoom_factor = "Использовать максимальный коэффициент увеличения",
+  posture_after_action_attempted = "Остановится или спешиться",
+  repair_when_interact_merchant = "Авто ремонт",
+  sell_junk_when_interact_merchant = "Автопродажа барахла",
+  indicate_accurate_buff_time = "Показывать время баффа 2ч как 120м",
+  indicate_item_id_and_level_on_tooltip = "Показать ID предмета и уровень во всплывающей подсказке",
+  indicate_item_vendor_price_on_tooltip = "Показать цену товара у торговца во всплывающей подсказке",
+  indicate_target_level_and_class_color_in_trade = "Показать уровень цели и цвет класса в торговом канале",
+  copy_url_in_chat = "Скопируйте URL-адрес щелчком мыши в сообщении чата",
+  log_chat = "Восстанавливайте сообщения чата до 64",
+  prevent_unit_combat_feedback = "Отключите журнал боя отображаемые в портрете игрока и питомца",
+  prevent_ui_error_message_during_combat = "Отключить ошибки интерфейса во время боя",
+  general = "Общее",
+  chat = "Чат",
+  reload_button = "Перезагрузить",
+  description = "введите /mp или / medusa_prelude для настройки"
+}
